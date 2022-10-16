d711ca978bc1e52039a132be9a0714fa26ac8cfa
nachat-ayoub <venomx2020x@gmail.com>
2022-10-16 18:18:34 +0100

    new commit

--- store/config.js
+++ store/config.js
@@ -6,7 +6,7 @@ const site = String(process.env.BASE_SITE_URL);
 
 // Site Config:
 const config = {
-  host: site?.split("/")?.at(-1) ?? "anime4up.vip",
+  host: "anime4up.vip",
   headers: {
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36",
