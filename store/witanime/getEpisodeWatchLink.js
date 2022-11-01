ec6aa2c8e793a55098af28aeae82b70faceedab9
nachat-ayoub <venomx2020x@gmail.com>
2022-11-01 01:22:05 +0100

    New Commit

--- store/witanime/getEpisodeWatchLink.js
+++ store/witanime/getEpisodeWatchLink.js
@@ -38,7 +38,7 @@ module.exports.getEpisodeWatchLink = async (slug) => {
 
         const { data } = await axios({
           method: "post",
-          url,
+          encodeURI(url),
           data: bodyFormData,
           headers: { "Content-Type": "multipart/form-data" },
         });
